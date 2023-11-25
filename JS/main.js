91574138ebcbbaf2217e58ba716e519bdf25bf29
Marta <marta.af.lab@gmail.com>
2023-11-25 11:30:46 +0100

    create JS

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+'use strict'
+//QUERISELECTOR
+
+const jsList = document.querySelector('.js-list');
+//VARIABLES
+document.querySelector('.js-list').innerHTML = 'const jsList';
+
+//EVENTOS
+//CODIGO CUANDO INICIA LA PAGINA
